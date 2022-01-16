346940159619ff9dc92f62ca3e95ee90d6057afa
EC2 Default User <ec2-user@ip-172-31-35-213.ec2.internal>
2022-01-16 14:24:56 +0000

    bookのcreate

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,25 @@
+class TodolistsController < ApplicationController
+  def new
+   @book = Book.new
+  end
+  
+def create
+
+    list = List.new(list_params)
+
+    list.save
+
+    redirect_to '/top'
+  end
+  
+  def index
+    @books = Book.all
+  end
+  
+  
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+ 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'todolists/new'
   resources :homes
   root to: 'homes#top'
   resources :books
-  
+  get 'todolists' => 'todolists#index'
+
+  get 'todolists/:id' => 'todolists#show', as: 'todolist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
