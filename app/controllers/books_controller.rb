0532939ec5097efc57d307aac0b855217a223a1e
EC2 Default User <ec2-user@ip-172-31-35-213.ec2.internal>
2022-01-18 12:55:18 +0000

    フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,37 +1,47 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.new  
+    @book = Book.new
     @books = Book.all
-    
+
   end
-  
+
   def show
-     @book = Book.find(params[:id])   
+     @book = Book.find(params[:id])
   end
 
   def new
 
   end
-  
+
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    if book.save
+     redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    else
+     render :new
+    end
   end
-  
+
+
   def edit
    @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id), notice: 'Book was successfully updated.'
   end
-    
+
+  def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   redirect_to '/books'
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
