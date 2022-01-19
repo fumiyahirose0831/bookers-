62c5b1f38f136e6a8d8971d442ff86d9863a85d7
EC2 Default User <ec2-user@ip-172-31-35-213.ec2.internal>
2022-01-19 11:58:09 +0000

    チェックOK

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,10 +14,10 @@ class BooksController < ApplicationController
   end
 
   def create
-      @books = Book.all
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
-     redirect_to book_path(book.id), notice: 'Book was successfully created.'
+     redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
      render :index
     end
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
    @books = Book.all    
     @book = Book.find(params[:id])
     if @book.update(book_params)
-    redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+    redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
     else
      render :show
     end
--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,4 @@ ActiveRecord::Schema.define(version: 2022_01_15_042217) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "migrates", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
